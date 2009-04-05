c4afc53f831ed5ec0a8d0b5595ef66b6a3fdc579
brianthecoder <wbsmith83@gmail.com>
2009-04-05 13:02:39 -0500

    gems

--- config/init.rb
+++ config/init.rb
@@ -1,5 +1,4 @@
 # Go to http://wiki.merbivore.com/pages/init-rb
-use_orm :datamapper
 use_test :rspec
 use_template_engine :haml
 
@@ -8,10 +7,6 @@ use_template_engine :haml
 #   :escape_html  => true,
 # }
 
-Merb::BootLoader.before_app_loads do
-  DataMapper.setup(:default, "couchdb://localhost:5984/davenport")
-end
- 
 Merb::BootLoader.after_app_loads do
   require Merb.root / 'lib' / 'sass-script-functions'
 end
